474f7ae86c0edf0c9e3c68e4376d29825ca7249b
ChebetMJ <jerutomercy53@gmail.com>
2023-08-15 04:26:33 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,20 @@
 /**
  * main - entry point
  *
- * Return: (0)
+ * Return: Always(0)
  */
 
 int main(void)
 {
-	-putchar('_');
-	-putchar('p');
-	-putchar('u');
-	-putchar('t');
-	-putchar('c');
-	-putchar('h');
-	-putchar('a');
-	-putchar('r');
-	-putchar('\n');
-	return (0);
+-putchar('_');
+-putchar('p');
+-putchar('u');
+-putchar('t');
+-putchar('c');
+-putchar('h');
+-putchar('a');
+-putchar('r');
+-putchar('\n');
+return (0);
 }
 
