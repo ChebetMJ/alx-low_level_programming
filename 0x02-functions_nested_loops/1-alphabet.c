99282aeb353f26c17541b772a8a9bac134859fdf
ChebetMJ <jerutomercy53@gmail.com>
2023-08-15 05:09:04 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * alphabet - print alphabet
+ *
+ * Return: void
+ */
+
+void alphabet (void)
+{
+	char n;
+	for(n = 'a'; n <= 'z'; n++)
+	_putchar(n);
+	_putchar('\n');
+
+}
